32f3401a33c67811496d48ff2271e12ddd2df669
Christian Dupuis <cd@atomist.com>
2017-12-06 21:16:25 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Demo12Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo12Application.class, args);
+	}
+}
